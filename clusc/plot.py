4012a8cd1292a6ff0b45e5f40c18b47504c706a7
WenlongShen <shenwl1988@gmail.com>
2020-08-26 16:58:46 +0800

    plot

--- clusc/plot.py
+++ clusc/plot.py
@@ -6,8 +6,11 @@ from clusc.utils import *
 import pygenometracks.tracks as pygtk
 import matplotlib.pyplot as plt
 import matplotlib.colors as mc
+import matplotlib.cm as mcm
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+
+default_colormap = 'gist_rainbow'
 
 def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 	if len(track_configs) < 2:
@@ -47,7 +50,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 			cobar.ax.yaxis.set_ticks_position('left')
 
 			if tk.properties.get('title') != None:
-				draw_title(ax, tk.properties['title'])
+				draw_track_title(ax, tk.properties['title'])
 
 		if infile_type == 'arcs' or infile_type == 'links':
 			if track_configs[i].get('section_name') == None:
@@ -56,7 +59,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 
 				if track_configs[i].get('color') == None \
 					or not is_colormap(track_configs[i].get('color')):
-					track_configs[i]['color'] = 'gist_rainbow'
+					track_configs[i]['color'] = default_colormap
 
 				if track_configs[i].get('clusters') == None:
 					tmp_pd = pd.read_csv(track_configs[i].get('file'), sep="\t", header=None)
@@ -88,7 +91,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 			cobar.ax.yaxis.set_ticks_position('left')
 
 			if tk.properties.get('title') != None:
-				draw_title(ax, tk.properties['title'])
+				draw_track_title(ax, tk.properties['title'])
 
 		if infile_type == 'bigwig' or infile_type == 'bw':
 			if track_configs[i].get('section_name') == None:
@@ -97,7 +100,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 			tk.plot(ax, region[0], region[1], region[2])
 
 			if tk.properties.get('title') != None:
-				draw_title(ax, tk.properties['title'])
+				draw_track_title(ax, tk.properties['title'])
 
 		if infile_type == 'gtf':
 			if track_configs[i].get('section_name') == None:
@@ -107,7 +110,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 			ax.set_yticks([])
 
 			if tk.properties.get('title') != None:
-				draw_title(ax, tk.properties['title'])
+				draw_track_title(ax, tk.properties['title'])
 
 		if infile_type == 'bed':
 			if track_configs[i].get('section_name') == None:
@@ -116,7 +119,7 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 
 				if track_configs[i].get('color') == None \
 					or not is_colormap(track_configs[i].get('color')):
-					track_configs[i]['color'] = 'gist_rainbow'
+					track_configs[i]['color'] = default_colormap
 
 				if track_configs[i].get('clusters') == None:
 					tmp_pd = pd.read_csv(track_configs[i].get('file'), sep="\t", header=None)
@@ -150,13 +153,72 @@ def plotting_tracks(region, track_configs, outfig_name, **kwargs):
 				cobar.ax.yaxis.set_ticks_position('left')
 
 			if tk.properties.get('title') != None:
-				draw_title(ax, tk.properties['title'])
+				draw_track_title(ax, tk.properties['title'])
 
 	plt.savefig(outfig_name)
 
 
 
-def plotting_circos(region, track_configs, outfig_name, **kwargs):
+def plotting_circos(circos_configs, outfig_name, **kwargs):
+	figsize=kwargs.get("figsize", (9, 9))
+	fig = plt.figure(figsize=figsize)
+	ax = fig.add_axes([0,0,1,1], polar=True)
+	ax.axis('off')
+	ax.set_ylim(0,max(figsize))
+
+	if circos_configs[0].get('type') != 'chrom':
+		raise Exception("Invalid first track... We need to configure chrom track first right now...")
+
+	chrom_regions = pd.read_csv(circos_configs[0].get('file'), sep="\t", names=['chrom','start','end'])
+	if not is_valid_chrom(chrom_regions):
+		raise Exception("Invalid first track... We do not support overlapped genomic regions right now...")
+
+	chrom_regions['length'] = chrom_regions['end']-chrom_regions['start']
+	total_len = chrom_regions['length'].sum()
+	gap = circos_configs[0].get('gap', np.pi/72)
+	len_per_theta = total_len/(np.pi*2-gap*chrom_regions.shape[0])
+
+	cumlen = [0] + list(chrom_regions['length'].cumsum())[:-1]
+	chrom_regions['theta_start'] = [np.pi/2-l/len_per_theta-gap*i for i,l in enumerate(cumlen)]
+	chrom_regions['theta_end'] = chrom_regions['theta_start']-chrom_regions['length']/len_per_theta
+
+	color,colormap,colorlist = get_colorlist(circos_configs[0].get('color'), 0, chrom_regions.shape[0]-1)
+
+	for index, row in chrom_regions.iterrows():
+		if color != None:
+			chrom_color = color[index%len(color)]
+		else:
+			chrom_color = colorlist.to_rgba(index)
+		ax.bar((row['theta_start']+row['theta_end'])/2,
+			circos_configs[0].get('width', 1),
+			color=chrom_color,
+			width=(row['theta_end']-row['theta_start']),
+			bottom=circos_configs[0].get('radius', 0.9)*max(figsize))
+
+	for i in range(1, len(circos_configs)):
+		if circos_configs[i].get('type') == 'highlight':
+			tmp_pd = pd.read_csv(circos_configs[i].get('file'), sep="\t", names=['chrom','start','end','name','score','strand'])
+			vmin = tmp_pd['score'].min()
+			vmax = tmp_pd['score'].max()
+
+			color,colormap,colorlist = get_colorlist(circos_configs[i].get('color'), vmin, vmax)
+
+			for index, row in tmp_pd.iterrows():
+				valid,ts,te = get_theta(chrom_regions, row, len_per_theta)
+				if not valid:
+					continue
+				if color != None:
+					bar_color = color[(int(row['score'])-vmin)%len(color)]
+				else:
+					bar_color = colorlist.to_rgba(row['score'])
+
+				ax.bar((ts+te)/2,
+					circos_configs[i].get('width', 1),
+					color=bar_color,
+					width=(te-ts),
+					bottom=circos_configs[i].get('radius', 0.9)*max(figsize))
+
+
 	plt.savefig(outfig_name)
 
 
@@ -167,7 +229,64 @@ def is_colormap(color_name):
 		return False
 
 
-def draw_title(ax, title):
+def colormap_to_rbg(colormap, vmin, vmax):
+	cNorm = mc.Normalize(vmin, vmax)
+	scalarMap = mcm.ScalarMappable(norm=cNorm, cmap=colormap)
+	return scalarMap
+
+
+def get_colorlist(color_name, vmin, vmax):
+	if color_name != None:
+		color = color_name
+		colormap = None
+		colorlist = None
+		if isinstance(color, str):
+			if is_colormap(color):
+				colormap = color
+				colorlist = colormap_to_rbg(colormap, vmin, vmax)
+				color = None
+			else:
+				colormap = None
+				colorlist = None
+				color = [color]
+	else:
+		colormap = default_colormap
+		colorlist = colormap_to_rbg(colormap, vmin, vmax)
+		color = None
+	return color,colormap,colorlist
+
+
+def draw_track_title(ax, title):
 	ax.text(1.1, 0.5, title, 
 		horizontalalignment='center', size='large', verticalalignment='center',
 		transform=ax.transAxes, wrap=True)
+
+
+def is_valid_chrom(chrom_pd):
+	for i in range(0, chrom_pd.shape[0]):
+		for j in range(i+1, chrom_pd.shape[0]):
+			if chrom_pd.loc[i,'chrom'] == chrom_pd.loc[j,'chrom'] \
+				and chrom_pd.loc[i,'start'] <= chrom_pd.loc[j,'end'] \
+				and chrom_pd.loc[j,'start'] <= chrom_pd.loc[i,'end']:
+				return False
+	return True
+
+
+def get_chromID(chrom_pd, regions_pd):
+	for index, row in chrom_pd.iterrows():
+		if regions_pd['chrom'] == row['chrom'] \
+			and regions_pd['start'] >= row['start'] \
+			and regions_pd['end'] <= row['end']:
+			return index
+	return -1
+
+
+def get_theta(chrom_pd, regions_pd, len_per_theta):
+	chromID = get_chromID(chrom_pd, regions_pd)
+	if chromID == -1:
+		return False,0,0
+	else:
+		ts = chrom_pd.loc[chromID, 'theta_start']-(regions_pd['start']-chrom_pd.loc[chromID, 'start'])/len_per_theta
+		te = chrom_pd.loc[chromID, 'theta_start']-(regions_pd['end']-chrom_pd.loc[chromID, 'start'])/len_per_theta
+		return True,ts,te
+	
---
+++ demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from clusc import *
+
+
+if __name__ == "__main__":
+
+	print("OK")
